1e85ef1242b2fbc6ba146c8d06a1238b7ce3bef2
Alsu Ismayilzade <Alsu.Ismayilzade@kibrit.tech>
2026-01-15 10:19:39 +0400

    add config

--- src/main/java/com/abb_tech/module_three/service/impl/CarServiceImpl.java
+++ src/main/java/com/abb_tech/module_three/service/impl/CarServiceImpl.java
@@ -1,5 +1,6 @@
 package com.abb_tech.module_three.service.impl;
 
+import com.abb_tech.module_three.config.AppConfig;
 import com.abb_tech.module_three.enums.CarAction;
 import com.abb_tech.module_three.exception.NotFoundException;
 import com.abb_tech.module_three.log.CarLogger;
@@ -9,6 +10,8 @@ import com.abb_tech.module_three.payload.response.CarResponse;
 import com.abb_tech.module_three.repository.CarListRepository;
 import com.abb_tech.module_three.service.CarService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -16,11 +19,16 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class CarServiceImpl implements CarService {
 
     private final CarLogger logger;
     private final CarListRepository carListRepository;
+    private final AppConfig appConfig;
+
+    @Value("${app.env}")
+    private String appEnv;
 
     private Car getCarById(Long id) {
         return carListRepository.findById(id)
@@ -40,6 +48,9 @@ public class CarServiceImpl implements CarService {
 
     @Override
     public CarResponse getById(Long id) {
+        if("prod".equals(appEnv)){
+            log.info("App is running on {} profile", appEnv);
+        }
         var car = getCarById(id);
         logger.log(CarAction.GET_BY_ID, car);
         return new CarResponse(car.getId(), car.getBrand(), car.getColor(), car.getModel());
@@ -47,6 +58,9 @@ public class CarServiceImpl implements CarService {
 
     @Override
     public List<CarResponse> getAll() {
+        if("dev".equals(appConfig.getEnv())){
+            log.info("App is running on {} profile", appConfig.getEnv());
+        }
         var cars = carListRepository.findAll();
         return cars.stream()
                 .map(car -> new CarResponse(car.getId(), car.getBrand(), car.getColor(), car.getModel()))
